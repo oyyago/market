9903e2134fa65e7c2c3bfa39a682e8be7cdfaf4b
oyyago <yyagoaraujo@gmail.com>
2023-08-22 02:18:50 -0400

    .

--- src/components/produtos.jsx
+++ src/components/produtos.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import EditProduto from './update';
 
 export function Delete() {
   const [produtos, setProdutos] = useState([]);
+  const [editProduto, setEditProduto] = useState(null);
 
   useEffect(() => {
-    const listarProdutos = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/produtos');
-        const data = await response.json();
-        setProdutos(data.dados);
-      } catch (error) {
-        console.error('Erro:', error);
-      }
-    };
-
-
     listarProdutos();
   }, []);
+
+  const listarProdutos = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/produtos');
+      const data = await response.json();
+      setProdutos(data.dados);
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  };
 
   const excluirProduto = async (produto_id) => {
     try {
@@ -25,15 +26,46 @@ export function Delete() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ id: produto_id }), 
+        body: JSON.stringify({ id: produto_id }),
       });
       if (response.ok) {
         setProdutos(produtos.filter((produto) => produto.produto_id !== produto_id));
-      } 
+      }
     } catch (error) {
       console.error('Erro:', error);
     }
   };
+
+  const iniciarEdicao = (produto) => {
+    setEditProduto(produto);
+  };
+
+  const salvarEdicao = async (dadosEditados) => {
+    try {
+      const response = await fetch('http://localhost:5000/produtos/patch', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nome: dadosEditados.nome_produto,
+          preco: dadosEditados.preco_unidade,
+          id: editProduto.produto_id,
+        }),
+      });
+      if (response.ok) {
+        setEditProduto(null);
+        listarProdutos();
+      }
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  };
+
+  const cancelarEdicao = () => {
+    setEditProduto(null);
+  };
+
   return (
     <>
       <div>
@@ -44,12 +76,20 @@ export function Delete() {
               <li>id produto: {produto.produto_id}</li>
               <li>{produto.nome_produto}</li>
               <li>Preço: {produto.preco_unidade}</li>
-              
               <button onClick={() => excluirProduto(produto.produto_id)}>Excluir</button>
+              <button onClick={() => iniciarEdicao(produto)}>Editar</button>
             </div>
           ))}
         </ul>
       </div>
+
+      {editProduto && (
+        <EditProduto
+          produto={editProduto}
+          onSave={salvarEdicao}
+          onCancel={cancelarEdicao}
+        />
+      )}
     </>
   );
 }
