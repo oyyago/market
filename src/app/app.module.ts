485598f5a7653fce7df0bbdd2f0abec37ecf07a9
oyyago <yyagoaraujo@gmail.com>
2023-10-14 03:48:58 -0400

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 import { FormsModule } from '@angular/forms';
+import { sideBarComponent } from './components/sideBar/sideBar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    sideBarComponent
   ],
   imports: [
     BrowserModule,
